a98e47c07ee3943a295089d07eb4c828daad44b0
Bủ Bủ <chaungocbuudang2003@gmail.com>
2023-11-16 20:11:20 +0700

    update navbar

--- screens/Log.jsx
+++ screens/Log.jsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Profile = () => {
+const Log = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>Log</Text>
     </View>
   );
 };
 
-export default Profile;
+export default Log;
 
 const styles = StyleSheet.create({});
--- screens/index.js
+++ screens/index.js
@@ -1,10 +1,9 @@
-import Home from "./Home"
-import Profile from "./Profile"
-import Search from "./Search"
-
+import ChatBot from "./ChatBot";
+import Home from "./Home";
+import Log from "./Log";
+import Shop from "./Shop";
+import Profile from "./Profile";
+import Search from "./Search";
 // Export to use in other parent directories
-export { 
-    Home,
-    Profile,
-    Search
-}
+
+export { ChatBot, Home, Profile, Search, Log, Shop };
