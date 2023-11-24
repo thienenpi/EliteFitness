fd5b5b5d9f1421ff40c40474e56923a4ba4a3e41
Bủ Bủ <chaungocbuudang2003@gmail.com>
2023-11-24 23:32:18 +0700

    dev log

---
+++ screens/styles/log.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { SIZES, COLORS } from "../../constants";
+
+const styles = StyleSheet.create({
+  container: {
+    height: SIZES.height,
+    backgroundColor: COLORS.primaryBg,
+  },
+
+  header: {
+    paddingHorizontal: 40,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 60,
+    flexDirection: "row",
+  },
+  userTxt: {
+    fontFamily: "rufner",
+    color: COLORS.text,
+    fontSize: 16,
+  },
+});
+
+export default styles;
