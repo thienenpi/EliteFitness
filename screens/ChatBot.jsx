41030cad2accf60e6ee905f8a0e230008e1857a2
ThienNguyen <thienenpi280220@gmail.com>
2023-12-09 15:12:51 +0700

    update chatbot

--- screens/ChatBot.jsx
+++ screens/ChatBot.jsx
@@ -4,7 +4,7 @@ import styles from "./styles/chatBot.style"
 import { GiftedChat } from "react-native-gifted-chat"
 import axios from "axios"
 
-const OPENAI_API_KEY = "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"
+const OPENAI_API_KEY = "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"
 // const openai = new OpenAI({
 //   apiKey: OPENAI_API_KEY,
 //   baseURL: "https://api.openai.com/v1/chat/completions",
@@ -23,38 +23,44 @@ const ChatBot = () => {
         GiftedChat.append(previousMessages, userMessage)
       )
       const messageText = userMessage.text.toLowerCase()
-    //   const keywords = [
-    //     "exercise",
-    //     "product",
-    //     "gym",
-    //     "muscle",
-    //     "exercises",
-    //     "products",
-    //     "muscles",
-    //   ]
+      //   const keywords = [
+      //     "exercise",
+      //     "product",
+      //     "gym",
+      //     "muscle",
+      //     "exercises",
+      //     "products",
+      //     "muscles",
+      //   ]
 
-    //   if (!keywords.some((keyword) => messageText.includes(keyword))) {
-    //     // If the message does not contain any gym-related keywords, respond with a default message
-    //     const botMessage = {
-    //       _id: new Date().getTime() + 1,
-    //       text: "I'm your Elite chatbot, please ask me anything relate to gym and exercise",
-    //       createdAt: new Date(),
-    //       user: {
-    //         _id: 2,
-    //         name: "Elite chatbot",
-    //       },
-    //     }
-    //     setMessages((previousMessages) =>
-    //       GiftedChat.append(previousMessages, botMessage)
-    //     )
-    //     return
-    //   }
+      //   if (!keywords.some((keyword) => messageText.includes(keyword))) {
+      //     // If the message does not contain any gym-related keywords, respond with a default message
+      //     const botMessage = {
+      //       _id: new Date().getTime() + 1,
+      //       text: "I'm your Elite chatbot, please ask me anything relate to gym and exercise",
+      //       createdAt: new Date(),
+      //       user: {
+      //         _id: 2,
+      //         name: "Elite chatbot",
+      //       },
+      //     }
+      //     setMessages((previousMessages) =>
+      //       GiftedChat.append(previousMessages, botMessage)
+      //     )
+      //     return
+      //   }
 
       // if the message contains gym-related keywords, fetch a answer from the API and response with it
       const response = await axios.post(
-        "https://api.openai.com/v1/engines/text-davinci-003/completions",
+        "https://api.openai.com/v1/chat/completions",
         {
-          prompt: messageText,
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: messageText,
+            },
+          ],
           max_tokens: 1200,
           temperature: 0.2,
           n: 1,
@@ -66,14 +72,13 @@ const ChatBot = () => {
           },
         }
       )
-    
-      const completion = response.data
-      console.log(completion)
 
-      const products = completion.choices[0].text.trim()
+      const completion = response.data
+
+      const content = completion.choices[0].message.content
       const botMessage = {
         _id: new Date().getTime() + 1,
-        text: products,
+        text: content,
         createdAt: new Date(),
         user: {
           _id: 2,
