980cf92f004eff9af14695f87cf7230017d027d3
Bủ Bủ <chaungocbuudang2003@gmail.com>
2023-12-21 10:10:10 +0700

    dev search product

--- screens/Shop.jsx
+++ screens/Shop.jsx
@@ -1,20 +1,27 @@
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles/shop.style";
 import SearchBar from "../components/shop/search";
 import { COLORS } from "../constants";
 import Slider from "../components/shop/slider";
 import ProductList from "../components/shop/productList";
+import useFetch from "../hook/useFetch";
 
 const Shop = () => {
+  const [productList, setProductList] = useState(null)
+
+  const updateProducts = (newProductList) => {
+    setProductList(newProductList)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.shopContainer}>
         {/* Header */}
         <View>
           <TouchableOpacity>
-            <SearchBar />
+            <SearchBar onUpdateProductList={updateProducts} />
           </TouchableOpacity>
         </View>
         <View
@@ -42,7 +49,7 @@ const Shop = () => {
 
       <ScrollView>
         <Slider />
-        <ProductList />
+        <ProductList items={productList}/>
       </ScrollView>
     </View>
   );
