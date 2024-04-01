49ef5945a54ac3e72a179869dae42d9f25e57131
Bủ Bủ <chaungocbuudang2003@gmail.com>
2024-04-01 16:31:04 +0700

    dev register screen

--- client/screens/Login.jsx
+++ client/screens/Login.jsx
@@ -1,22 +1,21 @@
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import React, { useContext, useEffect, useState } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Text,
   TouchableOpacity,
   View,
   TouchableWithoutFeedback,
+  TextInput,
   Keyboard,
-} from 'react-native';
-import {
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
-
-import CustomButton from '../components/CustomButton';
-import InputField from '../components/InputField';
-import { AuthContext } from '../context/AuthContext';
-import { styles1, styles2 } from './styles/login.style';
-import { COLORS } from '../constants';
+} from "react-native";
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+import LinearGradient from "react-native-linear-gradient";
+import CustomButton from "../components/CustomButton";
+import InputField from "../components/InputField";
+import { AuthContext } from "../context/AuthContext";
+import { styles1, styles2, googleButton } from "./styles/login.style";
+import { COLORS } from "../constants";
 
 const hideKeyboard = () => {
   Keyboard.dismiss();
@@ -25,8 +24,16 @@ const hideKeyboard = () => {
 const Login = () => {
   const navigation = useNavigation();
   const { login, loginWithGoogle } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //show password
+  const [inputType, setInputType] = useState("password");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+    setInputType(showPassword ? "password" : "default");
+  };
 
   const handleSubmit = () => {
     const data = {
@@ -39,81 +46,130 @@ const Login = () => {
 
   return (
     <TouchableWithoutFeedback onPress={hideKeyboard}>
-      <View style={styles1.container}>
-        <Text style={styles1.headerText}>LOG IN</Text>
-        <View style={{ height: 42 }}></View>
-        <InputField
-          icon={
-            <Ionicons
-              name="mail"
-              color={COLORS.secondary500}
-              size={24}
-            ></Ionicons>
-          }
-          styles={styles1}
-          label={'Email'}
-          keyboardType={'email-address'}
-          value={email}
-          onChangeText={(text) => {
-            setEmail(text);
-          }}
-          onSubmitEditing={handleSubmit}
-        ></InputField>
-        <View style={{ height: 20 }}></View>
-        <InputField
-          icon={<Ionicons name="keypad" size={24}></Ionicons>}
-          styles={styles1}
-          label={'Password'}
-          inputType={'password'}
-          value={password}
-          onChangeText={(text) => {
-            setPassword(text);
-          }}
-          onSubmitEditing={handleSubmit}
-        ></InputField>
-        <View style={{ height: 42 }}></View>
-        <CustomButton
-          styles={styles1}
-          isValid={true}
-          label={'LOGIN'}
-          onPress={() => {
-            handleSubmit();
-          }}
-        ></CustomButton>
-        <View
-          style={{
-            height: 20,
-            marginTop: 28,
-            display: 'flex',
-            flexDirection: 'row',
-            gap: 32,
-          }}
-        >
-          <Text style={styles1.smallText}>Don't have an account yet?</Text>
-          <TouchableOpacity
-            isValid={true}
+      <LinearGradient
+        colors={["#0E403D", "#00B7AF"]}
+        style={styles1.linearGradient}
+      >
+        <View style={styles1.container}>
+          <Text style={styles1.headerText}>LOG IN</Text>
+          <View style={{ height: 42 }}></View>
+          <InputField
+            icon={
+              <Ionicons name="mail" color={COLORS.primary} size={24}></Ionicons>
+            }
             styles={styles1}
-            onPress={() => navigation.navigate('Register')}
+            label={"Email"}
+            keyboardType={"email-address"}
+            value={email}
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+            onSubmitEditing={handleSubmit}
+          ></InputField>
+          <View style={{ height: 20 }}></View>
+          {/* <InputField
+            icon={
+              <Ionicons
+                name="keypad"
+                size={24}
+                color={COLORS.primary}
+              ></Ionicons>
+            }
+            styles={styles1}
+            label={"Password"}
+            inputType={"password"}
+            value={password}
+            secureTextEntry={!showPassword}
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+            onSubmitEditing={handleSubmit}
           >
-            <Text style={styles1.smallText}>Sign up</Text>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity
+              onPress={toggleShowPassword}
+              style={styles1.iconContainer}
+            >
+              <Ionicons
+                name={showPassword ? "eye-off" : "eye"}
+                size={24}
+                color="black"
+              />
+            </TouchableOpacity>
+          </InputField> */}
+          <View>
+            <InputField
+              icon={
+                <Ionicons
+                  name="keypad"
+                  size={24}
+                  color={COLORS.primary}
+                ></Ionicons>
+              }
+              styles={styles1}
+              label={"Password"}
+              // inputType={"password"}
+              inputType={inputType}
+              value={password}
+              onChangeText={(text) => {
+                setPassword(text);
+              }}
+              onSubmitEditing={handleSubmit}
+            />
+            <TouchableOpacity
+              onPress={toggleShowPassword}
+              style={styles1.iconContainer}
+            >
+              <Ionicons
+                name={showPassword ? "eye-off" : "eye"}
+                size={24}
+                color={COLORS.primary}
+              />
+            </TouchableOpacity>
+          </View>
 
-        <View style={{ height: 20 }}></View>
-        <Text style={styles1.forgotPassText}>Forgot password?</Text>
+          <View style={{ height: 42 }}></View>
+          <CustomButton
+            styles={styles1}
+            isValid={true}
+            label={"LOGIN"}
+            onPress={() => {
+              handleSubmit();
+            }}
+          ></CustomButton>
+          <View
+            style={{
+              height: 20,
+              marginTop: 28,
+              display: "flex",
+              flexDirection: "row",
+              gap: 32,
+            }}
+          >
+            <Text style={styles1.smallText}>Don't have an account yet?</Text>
+            <TouchableOpacity
+              isValid={true}
+              styles={styles1}
+              onPress={() => navigation.navigate("Register")}
+            >
+              <Text style={styles1.smallText}>Sign up</Text>
+            </TouchableOpacity>
+          </View>
 
-        <View
-          style={{
-            height: 20,
-            marginTop: 28,
-            marginBottom: 12,
-            width: '80%',
-            borderTopColor: COLORS.exerciseBg,
-            borderTopWidth: 1,
-          }}
-        ></View>
+          <View style={{ height: 20 }}></View>
+          <Text style={styles1.forgotPassText}>Forgot password?</Text>
 
-        {/* <CustomButton
+          <View
+            style={{
+              height: 20,
+              marginTop: 28,
+              marginBottom: 12,
+              width: "80%",
+              borderTopColor: COLORS.exerciseBg,
+              borderTopWidth: 1,
+            }}
+          ></View>
+
+          {/* <CustomButton
           icon={<Ionicons name="logo-google" size={16}></Ionicons>}
           styles={styles2}
           isValid={true}
@@ -122,12 +178,14 @@ const Login = () => {
             handleSubmit();
           }}
         ></CustomButton> */}
-        <GoogleSigninButton
-          size={GoogleSigninButton.Size.Standard}
-          color={GoogleSigninButton.Color.Light}
-          onPress={loginWithGoogle}
-        ></GoogleSigninButton>
-      </View>
+          <GoogleSigninButton
+            size={GoogleSigninButton.Size.Standard}
+            color={GoogleSigninButton.Color.Light}
+            onPress={loginWithGoogle}
+            style={googleButton.btnContainer}
+          ></GoogleSigninButton>
+        </View>
+      </LinearGradient>
     </TouchableWithoutFeedback>
   );
 };
