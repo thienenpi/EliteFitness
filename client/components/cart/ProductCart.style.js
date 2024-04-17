25835190ba7b1f0a1b8a2f7e349dea114760f91c
ThienNguyen <thienenpi280220@gmail.com>
2024-04-17 12:22:13 +0700

    feat: ProductCartItem.jsx

--- client/components/cart/ProductCart.style.js
+++ client/components/cart/ProductCart.style.js
@@ -1,16 +1,77 @@
 import { StyleSheet } from "react-native";
 import { SIZES, COLORS, TEXTS } from "../../constants";
+import CheckBox from "@react-native-community/checkbox";
 
 const styles = StyleSheet.create({
   cartItemContainer: {
-    height: 96,
+    paddingHorizontal: 12,
+    height: 112,
+    alignContent: "center",
     flexDirection: "row",
     borderWidth: 1,
     borderColor: COLORS.text,
     padding: 8,
     marginHorizontal: 12,
     borderRadius: 12,
+    gap: 12,
   },
+  CheckBox:{
+    justifyContent:"center",
+    alignContent:"center",
+  },
+
+  productInfoContainer:{
+    gap: 4,
+    paddingVertical: 4,
+  },
+  productName:{
+    color: COLORS.primary,
+    fontSize: TEXTS.bodyHeavy.fontSize,
+    fontFamily: TEXTS.bodyHeavy.fontFamily,
+  },
+  productCategory:{
+    color: COLORS.text,
+    fontSize: TEXTS.bodySmall.fontSize,
+    fontFamily: TEXTS.bodySmall.fontFamily,
+  },
+  productPriceContainer:{
+    flexDirection: "row",
+    width: "80%",
+    alignContent: "center",
+    justifyContent: "space-between",
+},
+  productPrice:{
+    color: COLORS.secondary,
+    marginTop: 4,
+    fontSize: TEXTS.price.fontSize,
+    fontFamily: TEXTS.price.fontFamily,
+  },quantityContainer:{
+    flexDirection: "row",
+    gap: 12,
+    alignContent: "center",
+    borderColor: COLORS.text,
+    borderWidth: 1,
+    padding: 4,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    justifyContent:"space-between"
+  },
+    quantityBtn:{
+        color: COLORS.primary,
+        fontSize: TEXTS.bodyHeavy.fontSize,
+        fontFamily: TEXTS.bodyHeavy.fontFamily,
+    },
+    quantity:{
+        color: COLORS.text,
+        fontSize: TEXTS.bodySmall.fontSize,
+        fontFamily: TEXTS.bodySmall.fontFamily,
+        alignSelf: "center",
+    },
+    productImage:{
+        width: 96,
+        height: 96,
+        borderRadius: 8,
+    }
 });
 
 export default styles;
--- client/components/cart/ProductCartItem.jsx
+++ client/components/cart/ProductCartItem.jsx
@@ -1,31 +1,75 @@
 import React, { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, Image, TouchableOpacity, View, Alert } from "react-native";
 import CheckBox from "@react-native-community/checkbox";
 import styles from "./ProductCart.style";
 
 const ProductCartItem = () => {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const [quantity, setQuantity] = useState(1); // Initial quantity is 1
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    } else {
+      // If quantity is already 1, prompt for deletion
+      Alert.alert(
+        "Delete Item",
+        "Are you sure you want to delete this item?",
+        [
+          {
+            text: "Cancel",
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => deleteItem() },
+        ],
+        { cancelable: false }
+      );
+    }
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const deleteItem = () => {
+    // Implement deletion logic here
+    // For now, let's just log a message
+    console.log("Item deleted");
+  };
 
   return (
     <TouchableOpacity style={styles.cartItemContainer}>
-      <View style={styles.productInfoContainer}>
-        <Text style={styles.productName}>Tên sản phẩm</Text>
-        <Text style={styles.productCategory}>Phân loại sản phẩm</Text>
-        <View style={styles.productPriceContainer}>
-          <Text style={styles.productPrice}>Giá SP</Text>
-          <TouchableOpacity style={styles.quantityContainer}>
-            <Text style={styles.quantity}>SL</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <TouchableOpacity onPress={() => setToggleCheckBox(!toggleCheckBox)}>
+      <TouchableOpacity
+        style={styles.CheckBox}
+        onPress={() => setToggleCheckBox(!toggleCheckBox)}
+      >
         <CheckBox
+          style={{ height: 20, width: 20 }}
           disabled={false}
           value={toggleCheckBox}
           onValueChange={(newValue) => setToggleCheckBox(newValue)}
         />
       </TouchableOpacity>
+      <Image
+        style={styles.productImage}
+        source={require("../../assets/images/products/whey.png")}
+      />
+      <View style={styles.productInfoContainer}>
+        <Text style={styles.productName}>Tên sản phẩm</Text>
+        <Text style={styles.productCategory}>Phân loại sản phẩm</Text>
+        <View style={styles.productPriceContainer}>
+          <Text style={styles.productPrice}>Giá SP</Text>
+          <View style={styles.quantityContainer}>
+            <TouchableOpacity onPress={decreaseQuantity}>
+              <Text style={styles.quantityBtn}>-</Text>
+            </TouchableOpacity>
+            <Text style={styles.quantity}>{quantity}</Text>
+            <TouchableOpacity onPress={increaseQuantity}>
+              <Text style={styles.quantityBtn}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 };
